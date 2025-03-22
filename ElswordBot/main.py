dc6debfab4187f928a1b56e1d80403be0a830e33
xhexlabx <tee.hemjinda.work@gmail.com>
2025-03-23 03:25:48 +0700

    update

--- ElswordBot/main.py
+++ ElswordBot/main.py
@@ -1,16 +1,20 @@
 import keyboard
 from ultralytics import YOLO
-from ElswordBot.modules import track_character
+from ElswordBot.modules import track_character, list_window_titles, activate_window_and_press
+
 
 def main():
     """Initialize and run the Elsword Bot."""
-    model = YOLO('./models/best.pt')
+    # model = YOLO('./models/best.pt')
+    #
+    # print("Elsword Bot is running... 🤖")
+    # keyboard.wait("1")  # Wait for user to start
+    # print("Tracking started!")
+    #
+    # track_character(model=model, delay=1.0)
 
-    print("Elsword Bot is running... 🤖")
-    keyboard.wait("1")  # Wait for user to start
-    print("Tracking started!")
-
-    track_character(model=model, delay=1.0)
+    list_window_titles()
+    activate_window_and_press(window_title="Saltfish" , key="k")
 
 if __name__ == "__main__":
     main()
