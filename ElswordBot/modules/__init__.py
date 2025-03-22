19440c98b622bc6fc7a552666f222c1db8172821
xhexlabx <tee.hemjinda.work@gmail.com>
2025-03-23 03:25:35 +0700

    Update __init__.py

--- ElswordBot/modules/__init__.py
+++ ElswordBot/modules/__init__.py
@@ -1,4 +1,6 @@
 from .get_character_position import get_character_position
 from .track_character import track_character
+from .list_window_titles import list_window_titles
+from .activate_window_and_press import activate_window_and_press
 
-__all__ = ['get_character_position'  , 'track_character']
+__all__ = ['get_character_position'  , 'track_character' , 'list_window_titles' , 'activate_window_and_press']
