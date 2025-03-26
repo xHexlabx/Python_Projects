c8282ec2a00d0a1c66c77128ad3a7ddbdfe0aa95
xhexlabx <tee.hemjinda.work@gmail.com>
2025-03-26 16:26:02 +0700

    .

--- TypeBreaker/main.py
+++ TypeBreaker/main.py
@@ -1,6 +1,6 @@
 import time
 
-from modules import ocr, type, data_screen_capture, screen_capture
+from modules import ocr, type, screen_capture
 import keyboard
 
 SCREEN_POSITION = {
@@ -8,18 +8,10 @@ SCREEN_POSITION = {
     "y1": 800,
     "x2": 2600,
     "y2": 900,
-    "interval_seconds": 0.5,
-    "output_folder": "./images"
-}
-
-SCREEN_POSITION_2 = {
-    "x1": 432,
-    "y1": 800,
-    "x2": 2600,
-    "y2": 900,
 }
 
 def main():
+
     print("TypeBreaker is ready. Press Enter to start, and press Enter again to stop.")
     keyboard.wait("enter")  # Wait for first Enter press to start
     print("TypeBreaker is running 🤖")
@@ -31,15 +23,13 @@ def main():
             print("TypeBreaker is stopping...")
             break
 
-        screenshot = screen_capture(**SCREEN_POSITION_2)
+        screenshot = screen_capture(**SCREEN_POSITION)
         text = ocr(screenshot)
         type(text)
 
         time.sleep(1)
         keyboard.press_and_release('tab+enter')
         time.sleep(1)
-
-
 
 if __name__ == "__main__":
     main()
--- TypeBreaker/modules/__init__.py
+++ TypeBreaker/modules/__init__.py
@@ -1,6 +1,5 @@
 from .ocr import ocr
 from .type import type
-from .data_screen_capture import data_screen_capture
 from .screen_capture import screen_capture
 
-__all__ = ["ocr" , "type" , "data_screen_capture" , "screen_capture"]
+__all__ = ["ocr" , "type" , "screen_capture"]
